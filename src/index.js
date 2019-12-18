bdb7c97ce14595dac2091990d5f171ebde4f6ec7
Donna Galletta <donna.galletta@isobar.com>
2019-12-18 12:45:18 +1100

    update all

--- src/index.js
+++ src/index.js
@@ -4,3 +4,65 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+
+
+//nav
+$(document).ready(function(){
+    $(".hamburger-menu").click(function(){
+ 
+      $(this).toggleClass('animate');
+
+      $('.toggle-hidden').slideToggle('').removeClass( "hidden" );
+
+    })
+  });
+
+
+
+$(window).bind('mousewheel', function(event) {
+  if (event.originalEvent.wheelDelta >= 0) {
+    $(".add-active").removeClass("active");
+  }
+  else {
+    $(".add-active").addClass("active");
+  }
+});
+
+
+
+// Scroll Down Button
+(function() {
+  'use strict';
+
+  var btnScrollDown = document.querySelector('#scroll_down');
+
+  function scrollDown() {
+    var windowCoords = document.documentElement.clientHeight;
+    (function scroll() {
+      if (window.pageYOffset < windowCoords) {
+        window.scrollBy(0, 10);
+        setTimeout(scroll, 0);
+      }
+      if (window.pageYOffset > windowCoords) {
+        window.scrollTo(0, windowCoords);
+      }
+    })();
+  }
+
+  btnScrollDown.addEventListener('click', scrollDown);
+})();
+
+
+
+
+$(function($){
+  
+  //Video Poster image
+  $('.play').click(function() {
+    const video = '<iframe src="' + $('img').attr('data-video') + '"></iframe>';
+    $(this).replaceWith(video);
+  });
+
+});
+
